754c6ffb228ad2251c4711f1d88a472486ac35fd
Viet Hua <huaviet999@gmail.com>
2023-06-12 21:46:12 +0700

    Refactor build gradle dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,21 +48,29 @@ android {
 }
 
 dependencies {
-
-    implementation("androidx.core:core-ktx:1.8.0")
-    implementation(platform("org.jetbrains.kotlin:kotlin-bom:1.8.0"))
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
+    // Jetpack Compose Standard Dependencies
     implementation("androidx.activity:activity-compose:1.5.1")
     implementation(platform("androidx.compose:compose-bom:2022.10.00"))
     implementation ("androidx.compose.ui:ui")
     implementation ("androidx.compose.ui:ui-graphics")
     implementation ("androidx.compose.ui:ui-tooling-preview")
     implementation ("androidx.compose.material3:material3")
+
+    // Android Kotlin Extension Dependencies
+    implementation("androidx.core:core-ktx:1.8.0")
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom:1.8.0"))
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
+
+    // Unit Testing Dependencies
     testImplementation ("junit:junit:4.13.2")
+
+    // UI Testing Dependencies
     androidTestImplementation ("androidx.test.ext:junit:1.1.3")
     androidTestImplementation ("androidx.test.espresso:espresso-core:3.4.0")
     androidTestImplementation(platform("androidx.compose:compose-bom:2022.10.00"))
     androidTestImplementation ("androidx.compose.ui:ui-test-junit4")
+
+    // Jetpack Compose Debug Dependencies
     debugImplementation ("androidx.compose.ui:ui-tooling")
     debugImplementation ("androidx.compose.ui:ui-test-manifest")
 }
