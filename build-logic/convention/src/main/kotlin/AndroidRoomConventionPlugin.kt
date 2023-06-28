a5fabf64b8184f9a4d960e7343bd5a9831b704fd
Viet Hua <huaviet999@gmail.com>
2023-06-28 21:30:37 +0700

    Create database module and add Room dependencies

---
+++ build-logic/convention/src/main/kotlin/AndroidRoomConventionPlugin.kt
@@ -0,0 +1,20 @@
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.api.artifacts.VersionCatalogsExtension
+import org.gradle.kotlin.dsl.dependencies
+import org.gradle.kotlin.dsl.getByType
+
+class AndroidRoomConventionPlugin : Plugin<Project> {
+    override fun apply(target: Project) {
+        with(target) {
+            pluginManager.apply("com.google.devtools.ksp")
+
+            val libs = extensions.getByType<VersionCatalogsExtension>().named("libs")
+            dependencies {
+                add("implementation", libs.findLibrary("room.runtime").get())
+                add("implementation", libs.findLibrary("room.ktx").get())
+                add("ksp", libs.findLibrary("room.compiler").get())
+            }
+        }
+    }
+}
